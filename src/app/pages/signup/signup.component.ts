e0838f65a6ecca63039d4f17f21c09e1f87b6b17
Eissa Faheem <eissafaheem@gmail.com>
2024-06-26 08:33:09 +0530

    feat: input width host 100%

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { InputComponent } from '../../_shared/components/input/input.component';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ButtonComponent } from '../../_shared/components/button/button.component';
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [InputComponent],
+  imports: [InputComponent, ButtonComponent],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.scss'
 })
