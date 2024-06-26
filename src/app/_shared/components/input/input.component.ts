3f4fe46bf1861bee045222dcdcdde419eb21ed3d
Eissa Faheem <eissafaheem@gmail.com>
2024-06-26 07:05:47 +0530

    feat: input changes label placeholder inputs focus

--- src/app/_shared/components/input/input.component.ts
+++ src/app/_shared/components/input/input.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -10,14 +10,28 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrl: './input.component.scss'
 })
 export class InputComponent implements OnInit {
-  label: string = 'Full Name';
-  control = new FormControl();
-  id: string = ""
+  @Input() label: string = '';
+  @Input() control = new FormControl();
+  @Input() id: string = ""
+  @Input() placeholder: string = ""
+  isFocused = false;
 
   ngOnInit() {
     this.control.valueChanges.subscribe((res) => {
       console.log(res)
     })
+  }
+
+  focusInput(input: HTMLInputElement): void {
+    input.focus();
+  }
+
+  onFocus(): void {
+    this.isFocused = true;
+  }
+
+  onBlur(): void {
+    this.isFocused = false;
   }
 
 }
