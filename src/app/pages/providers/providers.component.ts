c5bac2bd19413244605c2976a1e0a1971a2a4d32
rizwankazi661 <rizwankazi661@gmail.com>
2024-06-29 11:58:43 +0530

    feat: Common component for section banner implemented

--- src/app/pages/providers/providers.component.ts
+++ src/app/pages/providers/providers.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { SectionBannerComponent } from '../_shared/components/section-banner/section-banner.component';
 
 @Component({
-  selector: 'app-providers',
-  standalone: true,
-  imports: [],
-  templateUrl: './providers.component.html',
-  styleUrl: './providers.component.scss'
+    selector: 'app-providers',
+    standalone: true,
+    imports: [SectionBannerComponent],
+    templateUrl: './providers.component.html',
+    styleUrl: './providers.component.scss'
 })
 export class ProvidersComponent {
 
