4327c4a22842972f9261ed18a278a2012e4d83f1
rizwankazi661 <rizwankazi661@gmail.com>
2024-06-29 12:18:18 +0530

    feat: How it works component made common

--- src/app/pages/patients/patients.component.ts
+++ src/app/pages/patients/patients.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { SectionBannerComponent } from '../_shared/components/section-banner/section-banner.component';
+import { HowItWorksComponent, StepsType } from '../_shared/components/how-it-works/how-it-works.component';
 
 @Component({
     selector: 'app-patients',
     standalone: true,
-    imports: [SectionBannerComponent],
+    imports: [SectionBannerComponent, HowItWorksComponent],
     templateUrl: './patients.component.html',
     styleUrl: './patients.component.scss'
 })
 export class PatientsComponent {
-
+    public stepsArray: StepsType[] = [
+        {
+            highlightedText: 'Sign up online',
+            normalText: 'and enter your information.',
+        },
+        {
+            normalText: 'We find your script at a pharmacy in your area.',
+        },
+        {
+            normalText: 'Send your doctor the pharmacy info and pick up your script!',
+        }
+    ]
 }
