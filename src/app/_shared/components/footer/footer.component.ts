64672d6cb939ad79d9aa6fe875c99cc90c087a15
rizwankazi661 <rizwankazi661@gmail.com>
2024-06-29 21:35:56 +0530

    feat: Footer navigation updated

--- src/app/_shared/components/footer/footer.component.ts
+++ src/app/_shared/components/footer/footer.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
+import { ViewportScroller } from '@angular/common';
+import APP_ROUTES from '../../constants/routes';
 
 @Component({
-  selector: 'app-footer',
-  templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+    selector: 'app-footer',
+    templateUrl: './footer.component.html',
+    styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent {
+    APP_ROUTES = APP_ROUTES;
 
+    constructor(private router: Router, private viewportScroller: ViewportScroller) {
+        this.router.events.subscribe((event: Event) => {
+            if (event instanceof NavigationEnd) {
+                this.viewportScroller.scrollToPosition([0, 0]);
+            }
+        });
+    }
 }
