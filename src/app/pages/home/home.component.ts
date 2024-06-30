0d9742e58e532ca2a4bc283b68f7b1b3dc51f922
rizwankazi661 <rizwankazi661@gmail.com>
2024-06-30 09:48:38 +0530

    feat: FAQ section component made common

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,29 +3,56 @@ import { ButtonComponent } from '../../_shared/components/button/button.componen
 import { SectionBannerComponent } from '../_shared/components/section-banner/section-banner.component';
 import { HowItWorksComponent, StepsType } from '../_shared/components/how-it-works/how-it-works.component';
 import { SpecificationSectionComponent } from '../_shared/components/specification-section/specification-section.component';
+import { CommonModule } from '@angular/common';
+import { FAQSectionComponent } from '../_shared/components/faq-section/faq-section.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [ButtonComponent, SectionBannerComponent, HowItWorksComponent, SpecificationSectionComponent],
+    imports: [ButtonComponent, SectionBannerComponent, HowItWorksComponent, SpecificationSectionComponent, FAQSectionComponent, CommonModule],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss'
 })
 export class HomeComponent {
     public faqs = [
         {
-            question: 'What is FindMyMed?',
+            question: 'How does it work?',
             answer: 'FindMyMed is a service provided by InStockRx to help you find your medications.',
             isOpen: false
         },
         {
-            question: 'How do I use FindMyMed?',
-            answer: 'You can use FindMyMed by searching for your medication and checking the availability.',
+            question: 'What radius do you search for medication?',
+            answer: 'We start in your immediate zip code, and move out to further zip codes until we locate a medication that works for you. If the pharmacy we find is too far away, we are happy to restart a search for you.',
             isOpen: false
         },
         {
-            question: 'Is there a cost to use FindMyMed?',
-            answer: 'FindMyMed is free to use for all patients.',
+            question: 'What happens if you find my meds, but they are out of stock before I can pick them up?',
+            answer: 'In this case, we are happy to restart your search at no additional charge, providing that you attempted to pick up the medications within a reasonable time frame. If you call the pharmacy and they tell you the meds are out of stock after we locate them, we are happy to restart a search as well.',
+            isOpen: false
+        },
+        {
+            question: 'Is this a subscription?',
+            answer: 'No! We only charge one-time, up front packages.You can purchase just one search, or buy multiple and save.',
+            isOpen: false
+        },
+        {
+            question: 'Do unused med searches expire?',
+            answer: 'Nope! Unused searches never expire. That way, if you are able to find your meds on your own one month, you can save your unused search for the next time you need us!',
+            isOpen: false
+        },
+        {
+            question: 'What happens if you can\'t find my medication?',
+            answer: 'We can either refund your search or you can save your search for another time later down the line.',
+            isOpen: false
+        },
+        {
+            question: 'How much does findMyMed cost?',
+            answer: 'Med searches are as low as $30 per search. Please visit our pricing page for more information.',
+            isOpen: false
+        },
+        {
+            question: 'How do I contact findMyMed?',
+            answer: 'You can contact us by texting or calling us at (617) 631-8894.',
             isOpen: false
         }
     ];
@@ -42,8 +69,4 @@ export class HomeComponent {
             normalText: 'Send your doctor the pharmacy info and pick up your script!',
         }
     ]
-
-    toggleAnswer(index: number) {
-        this.faqs[index].isOpen = !this.faqs[index].isOpen;
-    }
 }
--- src/app/pages/providers/providers.component.ts
+++ src/app/pages/providers/providers.component.ts
@@ -2,15 +2,49 @@ import { Component } from '@angular/core';
 import { SectionBannerComponent } from '../_shared/components/section-banner/section-banner.component';
 import { HowItWorksComponent, StepsType } from '../_shared/components/how-it-works/how-it-works.component';
 import { SpecificationSectionComponent } from '../_shared/components/specification-section/specification-section.component';
+import { FAQSectionComponent } from '../_shared/components/faq-section/faq-section.component';
 
 @Component({
     selector: 'app-providers',
     standalone: true,
-    imports: [SectionBannerComponent, HowItWorksComponent, SpecificationSectionComponent],
+    imports: [SectionBannerComponent, HowItWorksComponent, FAQSectionComponent, SpecificationSectionComponent],
     templateUrl: './providers.component.html',
     styleUrl: './providers.component.scss'
 })
 export class ProvidersComponent {
+    public faqs = [
+        {
+            question: 'What is insito medfinder?',
+            answer: 'Medfinder is a service that partners with providers to help their patients find their medications.',
+            isOpen: false
+        },
+        {
+            question: 'How does it work?',
+            answer: 'When a patient is having trouble finding their medications, providers or admin staff sends the patient an invite to insito medfinder, or share a link to allow the patient to sign up themselves. Next, the patient creates their account and enters their credit card information. Once the patient has set up their account, insito works to find a pharmacy near the patient that has their medications in stock. After locating the script, insito sends the pharmacy details to the provider and notifies the patient.',
+            isOpen: false
+        },
+        {
+            question: 'Is medfinder HIPAA compliant?',
+            answer: 'Insito Medfinder is fully HIPAA compliant and our database is fully encrypted to ensure that we keep your trust.',
+            isOpen: false
+        },
+        {
+            question: 'What is the success rate of finding medications?',
+            answer: 'Currently, insito medfinder has a 99% success rate of finding and filling scripts for the patients that we serve.',
+            isOpen: false
+        },
+        {
+            question: 'How much does medfinder cost?',
+            answer: 'Medfinder is paid for directly by the patient and our prices are as low as $30 per search. For more information on pricing please visit the pricing page',
+            isOpen: false
+        },
+        {
+            question: 'How do I sign up?',
+            answer: 'Follow the create your account to sign up on your own and start referring patients, or schedule a call with us to learn more.',
+            isOpen: false
+        }
+    ];
+
     public stepsArray: StepsType[] = [
         {
             normalText: 'We partner with your practice and act as an optional medication finding service for patients.',
