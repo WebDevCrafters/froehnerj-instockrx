830b6109a348a1c1fa56d2bf1e067c29d3b54fd2
rizwankazi661 <rizwankazi661@gmail.com>
2024-06-30 09:51:49 +0530

    Text Updated

--- src/app/pages/providers/providers.component.ts
+++ src/app/pages/providers/providers.component.ts
@@ -14,28 +14,28 @@ import { FAQSectionComponent } from '../_shared/components/faq-section/faq-secti
 export class ProvidersComponent {
     public faqs = [
         {
-            question: 'What is insito medfinder?',
-            answer: 'Medfinder is a service that partners with providers to help their patients find their medications.',
+            question: 'What is instockrx findmymeds?',
+            answer: 'findmymeds is a service that partners with providers to help their patients find their medications.',
             isOpen: false
         },
         {
             question: 'How does it work?',
-            answer: 'When a patient is having trouble finding their medications, providers or admin staff sends the patient an invite to insito medfinder, or share a link to allow the patient to sign up themselves. Next, the patient creates their account and enters their credit card information. Once the patient has set up their account, insito works to find a pharmacy near the patient that has their medications in stock. After locating the script, insito sends the pharmacy details to the provider and notifies the patient.',
+            answer: 'When a patient is having trouble finding their medications, providers or admin staff sends the patient an invite to instockrx findmymeds, or share a link to allow the patient to sign up themselves. Next, the patient creates their account and enters their credit card information. Once the patient has set up their account, instockrx works to find a pharmacy near the patient that has their medications in stock. After locating the script, instockrx sends the pharmacy details to the provider and notifies the patient.',
             isOpen: false
         },
         {
-            question: 'Is medfinder HIPAA compliant?',
-            answer: 'Insito Medfinder is fully HIPAA compliant and our database is fully encrypted to ensure that we keep your trust.',
+            question: 'Is findmymeds HIPAA compliant?',
+            answer: 'Instockrx Findmymeds is fully HIPAA compliant and our database is fully encrypted to ensure that we keep your trust.',
             isOpen: false
         },
         {
             question: 'What is the success rate of finding medications?',
-            answer: 'Currently, insito medfinder has a 99% success rate of finding and filling scripts for the patients that we serve.',
+            answer: 'Currently, instockrx findmymeds has a 99% success rate of finding and filling scripts for the patients that we serve.',
             isOpen: false
         },
         {
-            question: 'How much does medfinder cost?',
-            answer: 'Medfinder is paid for directly by the patient and our prices are as low as $30 per search. For more information on pricing please visit the pricing page',
+            question: 'How much does findmymeds cost?',
+            answer: 'FindMyMeds is paid for directly by the patient and our prices are as low as $30 per search. For more information on pricing please visit the pricing page',
             isOpen: false
         },
         {
