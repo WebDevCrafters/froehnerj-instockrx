361e076bd9454902bb6fd80e45a20a16e3fcf769
Eissa Faheem <eissafaheem@gmail.com>
2024-06-30 16:05:32 +0530

    feat: better infinite slider

--- src/app/pages/_shared/components/infinite-slider/infinite-slider.component.ts
+++ src/app/pages/_shared/components/infinite-slider/infinite-slider.component.ts
@@ -5,9 +5,16 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './infinite-slider.component.html',
-  styleUrl: './infinite-slider.component.scss'
+  styleUrl: './infinite-slider.component.scss',
 })
 export class InfiniteSliderComponent {
+  // infiniteSlideArray = [
+  //   {
+  //     name: 'Joey. R',
+  //     title: "I can't thank this guys enough",
+  //     description:
+  //       'I  was out of my medication for two months and this company getting my vyvanse for me within 24hrs was phenomenal. Their response time and understanding of my situation was top notch. I found them through tiktok and will definitely be using in the future.',
+  //   },
+  // ];
   infiniteSlideArray = [1, 2, 3, 5, 6, 4, 8];
-
 }
