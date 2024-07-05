1fdf0b5713427d38c97df05e437499c371ffc432
Eissa Faheem <eissafaheem@gmail.com>
2024-07-05 22:50:32 +0530

    feat: moved find my med in layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import APP_ROUTES from './_shared/constants/routes';
 import { FindMyMedsComponent } from './pages/find-my-meds/find-my-meds.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { SelfServiceComponent } from './pages/self-service/self-service.component';
+import { SigninComponent } from './pages/signin/signin.component';
 export const routes: Routes = [
     { path: APP_ROUTES._, loadChildren: () => import("./layout/layout.module").then(m => m.LayoutModule) },
-    { path: APP_ROUTES.findMyMeds, component: FindMyMedsComponent },
+
     { path: APP_ROUTES.selfService, component: SelfServiceComponent },
-    { path: APP_ROUTES.dashboard, component: DashboardComponent }
-    { path: APP_ROUTES.signin, component: DashboardComponent }
+    { path: APP_ROUTES.dashboard, component: DashboardComponent },
+    { path: APP_ROUTES.signin, component: SigninComponent }
 ];
--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -9,24 +9,27 @@ import { ProvidersComponent } from '../pages/providers/providers.component';
 import { LayoutComponent } from './layout.component';
 import { PrivacyComponent } from '../pages/privacy/privacy.component';
 import { TermsOfServiceComponent } from '../pages/terms-of-service/terms-of-service.component';
+import { FindMyMedsComponent } from '../pages/find-my-meds/find-my-meds.component';
 
 const routes: Routes = [
-    {
-        path: APP_ROUTES._, component: LayoutComponent,
-        children: [
-            { path: APP_ROUTES._, component: HomeComponent },
-            { path: APP_ROUTES.about, component: AboutUsComponent },
-            { path: APP_ROUTES.patients, component: PatientsComponent },
-            { path: APP_ROUTES.pricing, component: PricingComponent },
-            { path: APP_ROUTES.providers, component: ProvidersComponent },
-            { path: APP_ROUTES.privacy, component: PrivacyComponent },
-            { path: APP_ROUTES.termsOfService, component: TermsOfServiceComponent },
-        ]
-    },
+  {
+    path: APP_ROUTES._,
+    component: LayoutComponent,
+    children: [
+      { path: APP_ROUTES._, component: HomeComponent },
+      { path: APP_ROUTES.about, component: AboutUsComponent },
+      { path: APP_ROUTES.patients, component: PatientsComponent },
+      { path: APP_ROUTES.pricing, component: PricingComponent },
+      { path: APP_ROUTES.providers, component: ProvidersComponent },
+      { path: APP_ROUTES.privacy, component: PrivacyComponent },
+      { path: APP_ROUTES.termsOfService, component: TermsOfServiceComponent },
+      { path: APP_ROUTES.findMyMeds, component: FindMyMedsComponent },
+    ],
+  },
 ];
 
 @NgModule({
-    imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class LayoutRoutingModule { }
+export class LayoutRoutingModule {}
