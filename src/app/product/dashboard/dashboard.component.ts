cb409edad1ba52a0f0b4e52020975a04827b0ed9
Eissa Faheem <eissafaheem@gmail.com>
2024-07-06 10:31:37 +0530

    feat: dashboard left side static complete

--- src/app/product/dashboard/dashboard.component.ts
+++ src/app/product/dashboard/dashboard.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../_shared/components/header/header.component';
 import { ButtonComponent } from '../../_shared/components/button/button.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [HeaderComponent, ButtonComponent],
+  imports: [HeaderComponent, ButtonComponent, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  paymentCompleted: boolean = false;
 
 }
