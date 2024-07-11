dd22a769974c7fc2982a9d3a0b82189db475551f
Eissa Faheem <eissafaheem@gmail.com>
2024-07-11 21:22:50 +0530

    feat: landing page complete text change

--- src/app/webpage/pages/home/home.component.ts
+++ src/app/webpage/pages/home/home.component.ts
@@ -17,57 +17,57 @@ import { InfiniteSliderComponent } from '../_shared/components/infinite-slider/i
 export class HomeComponent {
     public faqs = [
         {
-            question: 'How does it work?',
-            answer: 'FindMyMeds is a service provided by InStockRx to help you find your medications.',
+            question: 'How does FindMyMeds work?',
+            answer: 'FindMyMeds, powered by InStockRx, aggregates and centralizes pharmacy inventories from to help you locate your hard-to-find medications quickly and efficiently.',
             isOpen: false
         },
         {
-            question: 'What radius do you search for medication?',
-            answer: 'We start in your immediate zip code, and move out to further zip codes until we locate a medication that works for you. If the pharmacy we find is too far away, we are happy to restart a search for you.',
+            question: 'What search radius does FindMyMeds use for medication?',
+            answer: 'We start by searching within 30 mile radius from your zip code and then expand as needed. If the pharmacy is too far away, we will request delivery at no cost.',
             isOpen: false
         },
         {
-            question: 'What happens if you find my meds, but they are out of stock before I can pick them up?',
-            answer: 'In this case, we are happy to restart your search at no additional charge, providing that you attempted to pick up the medications within a reasonable time frame. If you call the pharmacy and they tell you the meds are out of stock after we locate them, we are happy to restart a search as well.',
+            question: 'What happens if FindMyMeds finds my medication, but it\'s out of stock before I can pick it up?',
+            answer: 'If the medication is out of stock when you attempt to pick it up, we will restart your search at no additional charge, provided you attempted to pick up the medication within a reasonable time frame. If the pharmacy informs you that the meds are out of stock after we locate them, we will also restart the search.',
             isOpen: false
         },
         {
-            question: 'Is this a subscription?',
-            answer: 'No! We only charge one-time, up front packages.You can purchase just one search, or buy multiple and save.',
+            question: 'Is FindMyMeds a subscription service?',
+            answer: 'No, FindMyMeds offers one-time, upfront packages. You can purchase a single search or buy multiple searches at a discount.',
             isOpen: false
         },
         {
-            question: 'Do unused med searches expire?',
-            answer: 'Nope! Unused searches never expire. That way, if you are able to find your meds on your own one month, you can save your unused search for the next time you need us!',
+            question: 'Do unused medication searches from FindMyMeds expire?',
+            answer: 'No, unused searches never expire. If you find your meds on your own, you can save your unused search for the next time you need us.',
             isOpen: false
         },
         {
-            question: 'What happens if you can\'t find my medication?',
-            answer: 'We can either refund your search or you can save your search for another time later down the line.',
+            question: 'What happens if FindMyMeds can\'t find my medication?',
+            answer: 'If we can\'t locate your medication, you can choose to receive a refund or save your search for future use.',
             isOpen: false
         },
         {
-            question: 'How much does findMyMed cost?',
-            answer: 'Med searches are as low as $30 per search. Please visit our pricing page for more information.',
+            question: 'How much does FindMyMeds cost?',
+            answer: 'Medication searches start as low as $20 per search. Please visit our pricing page for more information.',
             isOpen: false
         },
         {
-            question: 'How do I contact findMyMed?',
-            answer: 'You can contact us by texting or calling us at (617) 631-8894.',
+            question: 'How do I contact FindMyMeds?',
+            answer: 'You can contact us by texting or calling (901) 609-8315.',
             isOpen: false
         }
     ];
 
     public stepsArray: StepsType[] = [
         {
-            highlightedText: 'Sign up',
-            normalText: '& Submit: Register and provide your prescription details.',
+            highlightedText: 'Sign Up Online:',
+            normalText: 'Create an account and enter your hard-to-find medication.',
         },
         {
-            normalText: 'We Search: Our team finds your medication at nearby pharmacies.',
+            normalText: 'We Find Your Med: We locate it at the closest pharmacy.',
         },
         {
-            normalText: 'Get Results: Receive timely updates. If we can\'t find it, your search is free or credited for future use.',
+            normalText: 'Pick Up Your Med: Receive a text when it\'s found, share the info with your doctor, and pick it up or arrange for delivery',
         }
     ]
 
