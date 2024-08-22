b00996087e82f2d91ae95e42c4713e01c280bd2e
rizwankazi661 <rizwankazi661@gmail.com>
2024-08-22 07:06:46 +0530

    feat: Medication Details Screen UI completed

--- src/app/product/dashboard/patient/_shared/medication-details/medication-details.component.ts
+++ src/app/product/dashboard/patient/_shared/medication-details/medication-details.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-medication-details',
-  standalone: true,
-  imports: [],
-  templateUrl: './medication-details.component.html',
-  styleUrl: './medication-details.component.scss'
+    selector: 'app-medication-details',
+    standalone: true,
+    imports: [],
+    templateUrl: './medication-details.component.html',
+    styleUrl: './medication-details.component.scss'
 })
-export class MedicationDetailsComponent {
 
+
+export class MedicationDetailsComponent {
+    /**
+     @todo Add tooltip, add ellipses, handle mutiple alternative medications
+    **/
 }
