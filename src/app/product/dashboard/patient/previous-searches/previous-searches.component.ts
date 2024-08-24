123d0b59fc0d4d693bc8e24167d19c845e095a8d
Eissa Faheem <eissafaheem@gmail.com>
2024-08-24 20:45:01 +0530

    feat: previous searches done

--- src/app/product/dashboard/patient/previous-searches/previous-searches.component.ts
+++ src/app/product/dashboard/patient/previous-searches/previous-searches.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SearchService } from '../../../../_core/services/search.service';
+import { SearchStatus } from '../../../_shared/interfaces/SearchStatus';
+import Search from '../../../_shared/interfaces/Search';
 
 @Component({
-  selector: 'app-previous-searches',
-  standalone: true,
-  imports: [],
-  templateUrl: './previous-searches.component.html',
-  styleUrl: './previous-searches.component.scss'
+    selector: 'app-previous-searches',
+    standalone: true,
+    imports: [],
+    templateUrl: './previous-searches.component.html',
+    styleUrl: './previous-searches.component.scss',
 })
-export class PreviousSearchesComponent {
+export class PreviousSearchesComponent implements OnInit {
+    previousSearches: Search[] = [];
+    constructor(private searchService: SearchService) {}
 
+    ngOnInit(): void {
+        this.getMyCompletedSearch();
+    }
+
+    getMyCompletedSearch() {
+        this.searchService.getMySearches(SearchStatus.NotStarted).subscribe({
+            next: (result) => {
+                this.previousSearches.push(...result);
+            },
+            error: (err) => {
+                console.log(err);
+            },
+        });
+    }
 }
