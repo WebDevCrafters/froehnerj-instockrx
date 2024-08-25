b751f457a9fd4408381709b530d5628d30c17bc3
Eissa Faheem <eissafaheem@gmail.com>
2024-08-25 13:46:34 +0530

    feat: added properties in search obj

--- src/app/product/_shared/interfaces/Search.ts
+++ src/app/product/_shared/interfaces/Search.ts
@@ -1,12 +1,14 @@
-import Medication from "./Medication";
-import { SearchStatus } from "./SearchStatus";
-
+import Medication from './Medication';
+import { SearchStatus } from './SearchStatus';
 
 interface Search {
     searchId?: string;
     patient: string;
     medication: Medication;
     status: SearchStatus;
+    zipCode: number;
+    location: Location;
+    prescriberName: string;
 }
 
 export default Search;
