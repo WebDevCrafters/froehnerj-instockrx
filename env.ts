d9c0d9b48ddbd926a3a5e2f164596974e29fac06
Eissa Faheem <eissafaheem@gmail.com>
2024-09-05 22:27:43 +0530

    feat: deployed urls

--- env.ts
+++ env.ts
@@ -2,5 +2,8 @@
     @todo: Urgent, have to add .env
  */
 
-    export const API_URL = "http://localhost:1000/api"
-    export const BASE_URL = "http://localhost:1000/"
+//  export const API_URL = "http://localhost:1000/api"
+//  export const BASE_URL = "http://localhost:1000/"
+
+export const BASE_URL = 'https://froehnerj-findmymeds-backend-1.onrender.com/';
+export const API_URL = BASE_URL + 'api';
