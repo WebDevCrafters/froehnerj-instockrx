2f4036de3e8daa707f0c907b76a2f73cea00cd20
Eissa Faheem <eissafaheem@gmail.com>
2024-09-26 21:24:42 +0530

    feat: env file

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    BASE_URL: "this is dev",
+    STRIPE_PK: "stripe pk"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    BASE_URL: "this is prod",
+    STRIPE_PK: "stripe pk"
+};
