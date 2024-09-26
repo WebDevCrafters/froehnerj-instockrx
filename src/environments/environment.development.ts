84338ea6887181e06530f743d04609ed5fc13f2c
Eissa Faheem <eissafaheem@gmail.com>
2024-09-26 22:24:39 +0530

    feat: added all configs and implementation for enviromnent

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,5 @@
 export const environment = {
-    production: false,
-    BASE_URL: 'http://localhost:1000',
-    STRIPE_PK:
-        'pk_test_51PXmuSRxTLGyRwslZXdQ6wIC0MQwowKVInmZBMt2VDEO6mYucXa9c8UgTyUQ87HbKkveFbelAZlyKYKKkg7dVlbR00GMY68bnJ',
+    production: true,
+    BASE_URL: import.meta.env['NG_APP_BASE_URL'],
+    STRIPE_PK: import.meta.env['NG_APP_STRIPE_PK']
 };
